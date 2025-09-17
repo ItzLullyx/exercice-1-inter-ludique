d9a96fa8203ec1fbe337239126deda68b00f7b7d
ItzLullyx <lighning801@gmail.com>
2025-09-17 14:08:29 -0400

    Song addition and tweaks

--- test/node_2d.gd
+++ test/node_2d.gd
@@ -15,43 +15,59 @@ var playing_purple := false
 @onready var anim_yellow: AnimatedSprite2D = $Yellow
 var playing_yellow := false
 
+@onready var song: AudioStreamPlayer2D = $CogworkDancers
+@onready var forest_sounds: AudioStreamPlayer2D = $ForestSounds
+
+func _ready() -> void:
+	forest_sounds.play()
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_A:
 		if playing_beige:
 			anim_beige.stop()
 			playing_beige = false
+			song.stop()
 		else:
 			anim_beige.play("default")
 			playing_beige = true
+			song.play()
 	
 	if event is InputEventKey and event.pressed and event.keycode == KEY_S:
 		if playing_green:
 			anim_green.stop()
 			playing_green = false
+			song.stop()
 		else:
 			anim_green.play("default")
 			playing_green = true
+			song.play()
 
 	if event is InputEventKey and event.pressed and event.keycode == KEY_D:
 		if playing_pink:
 			anim_pink.stop()
 			playing_pink = false
+			song.stop()
 		else:
 			anim_pink.play("default")
 			playing_pink = true
+			song.play()
 			
 	if event is InputEventKey and event.pressed and event.keycode == KEY_F:
 		if playing_purple:
 			anim_purple.stop()
 			playing_purple = false
+			song.stop()
 		else:
 			anim_purple.play("default")
 			playing_purple = true
+			song.play()
 			
 	if event is InputEventKey and event.pressed and event.keycode == KEY_G:
 		if playing_yellow:
 			anim_yellow.stop()
 			playing_yellow = false
+			song.stop()
 		else:
 			anim_yellow.play("default")
 			playing_yellow = true
+			song.play()
